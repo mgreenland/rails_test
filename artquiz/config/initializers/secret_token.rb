d64da2cd62fb1d0a69e6c648dd7889cfaf2930df
unknown <Matt@.(none)>
2011-02-05 14:19:09 -0500

    initial rails generation

---
+++ artquiz/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artquiz::Application.config.secret_token = '3596d4c7e58923f3925b6ac667a65ad70c8c493c9cc16209600cd36f8249c6a0b89a62e5db3de8a970e32ffaeb7d720e9a5024e2e922ff88b84cc1dfa1a9740c'
